9c45268de0ed37894cffe946df131b9708825517
Yuya ONUMA <yohnuma@gion-xy.net>
2015-03-05 23:24:07 +0900

    chap 3.4

--- chap3/js/spa.js
+++ chap3/js/spa.js
@@ -5,7 +5,7 @@
 var spa = (function() {
     "use strict";
     var initModule = function($container) {
-        $container.html('<h1 style="display: inline-block; margin: 25px;">hello world!</h1>');
+        spa.shell.initModule($container);
     };
 
     return {initModule: initModule};
