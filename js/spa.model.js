cadb658e568de91f1970d6fa32bd9648ed1905e6
Yuya ONUMA <yohnuma@gion-xy.net>
2015-03-08 19:02:03 +0900

    chap 5.4.3.1

--- js/spa.model.js
+++ js/spa.model.js
@@ -12,12 +12,18 @@ spa.model = function() {
         },
         stateMap = {
             anon_user: null,
+            cid_serial: 0,
             people_cid_map: {},
-            people_db: TAFFY()
+            people_db: TAFFY(),
+            user: null
         },
         isFakeData = true,
         personProto,
+        makeCid,
+        clearPeopleDB,
+        completeLogin,
         makePerson,
+        removePerson,
         people,
         initModule;
 
@@ -29,6 +35,34 @@ spa.model = function() {
         get_is_anon: function() {
             return this.cid === stateMap.anon_user.cid;
         }
+    };
+
+
+    makeCid = function() {
+        return 'c' + String(stateMap.cid_serial++);
+    };
+
+
+    clearPeopleDB = function() {
+        var user = stateMap.user;
+        stateMap.people_db = TAFFY();
+        stateMap.people_cid_map = {};
+        if (user) {
+            stateMap.people_db.insert(user);
+            stateMap.people_cid_map[user.cid] = user;
+        }
+    };
+
+
+    completeLogin = function(user_list) {
+        var user_map = user_list[0];
+        delete stateMap.people_cid_map[user_map.cid];
+        stateMap.user.cid = user_map._id;
+        stateMap.user.id = user_map._id;
+        stateMap.user.css_map = user_map.css_map;
+        stateMap.people_cid_map[user_map._id] = stateMap.user;
+
+        $.gevent.publish('spa-login', [stateMap.user]);
     };
 
 
@@ -58,10 +92,113 @@ spa.model = function() {
     };
 
 
-    people = {
-        get_db: function() { return stateMap.people_db; },
-        get_cid_map: function() { return stateMap.people_cid_map; }
+    removePerson = function(person) {
+        if (!person) {
+            return false;
+        }
+        if (person.id === configMap.anon_id) {
+            return false;
+        }
+
+        stateMap.people_db({cid: person.cid}).remove();
+        if (person.cid) {
+            delete stateMap.people_cid_map[person.cid];
+        }
+        return true;
     };
+
+
+    people = (function() {
+        var get_by_cid,
+            get_db,
+            get_user,
+            login,
+            logout;
+
+
+        /**
+         * Get a person object with provided unique id.
+         * @param cid cid of user
+         * @returns {*} selected person objectb
+         */
+        get_by_cid = function (cid) {
+            return stateMap.people_cid_map[cid];
+        };
+
+
+        /**
+         * Get the TaffyDB database of all the person objects
+         *     - including the current user - presorted.
+         * @returns database of persons
+         */
+        get_db = function () {
+            return stateMap.people_db;
+        };
+
+
+        /**
+         * Get the current user person object.
+         *     If the current user is not signed-in, an anonymous person object is returned.
+         * @returns current person object
+         */
+        get_user = function () {
+            return stateMap.user;
+        };
+
+
+        /**
+         * Login as the user with the provided user name.
+         *     The current user object is changed to reflect the new identity.
+         *     Successful completion of login publishes a 'spa-login' global custom event.
+         * @param name name of person
+         */
+        login = function (name) {
+            var sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
+
+            stateMap.user = makePerson({
+                cid: makeCid(),
+                css_map: {
+                    top: 25,
+                    left: 25,
+                    'background-color': '#8f8'
+                },
+                name: name
+            });
+
+            sio.on('userupdate', completeLogin);
+
+            sio.emit('adduser', {
+                cid: stateMap.user.cid,
+                css_map: stateMap.user.css_map,
+                name: stateMap.user.name
+            });
+        };
+
+
+        /**
+         * Revert the current user object to anonymous.
+         *     This method publishes a 'spa-logout' global custom event.
+         */
+        logout = function () {
+            var is_removed,
+                user = stateMap.user;
+
+            is_removed = removePerson(user);
+            stateMap.user = stateMap.anon_user;
+
+            $.gevent.publish('spa-logout', [user]);
+            return is_removed;
+        };
+
+
+        return {
+            get_by_cid: get_by_cid,
+            get_db: get_db,
+            get_user: get_user,
+            login: login,
+            logout: logout
+        };
+    }());
 
 
     initModule = function() {
