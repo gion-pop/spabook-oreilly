2f068cf056a4ea3f31dbfb44cdffbf3c3a2356b8
Yuya ONUMA <yohnuma@gion-xy.net>
2015-03-11 14:59:51 +0900

    chap 6.3.2

--- js/spa.fake.js
+++ js/spa.fake.js
@@ -73,7 +73,7 @@ spa.fake = (function() {
 
 
         emit_sio = function(msg_type, data) {
-            var person_map;
+            var person_map, i;
 
             if (msg_type === 'adduser' && callback_map.userupdate) {
                 setTimeout(function() {
@@ -108,6 +108,16 @@ spa.fake = (function() {
                     listchange_idto = undefined;
                 }
                 send_listchange();
+            }
+
+            if (msg_type === 'updateavatar' && callback_map.listchange) {
+                for (i = 0; i < peopleList.length; i++) {
+                    if (peopleList[i]._id === data.person_id) {
+                        peopleList[i].css_map = data.css_map;
+                        break;
+                    }
+                }
+                callback_map.listchange([peopleList]);
             }
         };
 
