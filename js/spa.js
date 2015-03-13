48ff5c607df4a9bc2cf6a4ba38b0f1090b1f5880
Yuya ONUMA <yohnuma@gion-xy.net>
2015-03-13 20:48:22 +0900

    chap 6.7

--- js/spa.js
+++ js/spa.js
@@ -5,6 +5,7 @@
 var spa = (function() {
     "use strict";
     var initModule = function($container) {
+        spa.data.initModule();
         spa.model.initModule();
         spa.shell.initModule($container);
     };
--- js/spa.model.js
+++ js/spa.model.js
@@ -18,7 +18,7 @@ spa.model = function() {
             user: null,
             is_connected: false
         },
-        isFakeData = true,
+        isFakeData = false,
         personProto,
         makeCid,
         clearPeopleDB,
