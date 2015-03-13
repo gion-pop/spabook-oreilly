68e53dd7ae962d3f24cd2beeab4bc3182bc3a4ae
Yuya ONUMA <yohnuma@gion-xy.net>
2015-03-14 00:25:34 +0900

    chap 7.3.1

--- webapp/app.js
+++ webapp/app.js
@@ -35,6 +35,37 @@ app.get('/', function(request, response) {
     response.redirect('/spa.html');
 });
 
+app.all('/user/*?', function(request, response, next) {
+    response.contentType('json');
+    next();
+});
+
+app.get('/user/list', function(request, response) {
+    response.send({title: 'user list'});
+});
+
+app.post('/user/create', function(request, response) {
+    response.send({title: 'user created'});
+});
+
+app.get('/user/read/:id([0-9]+)', function(request, response) {
+    response.send({
+        title: 'user with id ' + request.params.id + ' found'
+    });
+});
+
+app.get('/user/update/:id([0-9]+)', function(request, response) {
+    response.send({
+        title: 'user with id ' + request.params.id + ' updated'
+    });
+});
+
+app.get('/user/delete/:id([0-9]+)', function(request, response) {
+    response.send({
+        title: 'user with id ' + request.params.id + ' deleted'
+    });
+});
+
 
 server.listen(3000);
 console.log(
